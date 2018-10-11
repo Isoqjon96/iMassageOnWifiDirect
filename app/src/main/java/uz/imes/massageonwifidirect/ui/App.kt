b4d52bf23855a39b2c191d2e9b79d09b432e7773
IsoqHakimov <IsoqHakimov@users.noreply.github.com>
2018-10-12 03:58:40 +0500

    wifidirectadd

---
+++ app/src/main/java/uz/imes/massageonwifidirect/ui/App.kt
@@ -0,0 +1,12 @@
+package uz.imes.imessanger
+
+import android.app.Application
+
+/**
+ * Created by Isoq Hakimov on 10.10.2018.
+ */
+class App: Application() {
+    companion object {
+        lateinit var user:String
+    }
+}
