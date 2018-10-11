2cd18da20b6059d79282491c599c23ee696cc4a1
Kadiroff <jkadiroff@gmail.com>
2018-10-12 02:13:07 +0500

    ui1

--- app/src/main/java/uz/imes/massageonwifidirect/MainActivity.kt
+++ app/src/main/java/uz/imes/massageonwifidirect/MainActivity.kt
@@ -1,12 +1,20 @@
 package uz.imes.massageonwifidirect
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvJoinHost.setOnClickListener {
+            startActivity(Intent(this, WaitingActivity::class.java))
+        }
+        tvCreateHost.setOnClickListener {
+            startActivity(Intent(this, CreatedHostActivity::class.java))
+        }
     }
 }
