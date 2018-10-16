c56d57902ea5037075695bc2258a90a7910d2db1
Kadiroff <jkadiroff@gmail.com>
2018-10-16 15:28:49 +0500

    111

--- app/src/main/java/uz/imes/massageonwifidirect/ui/RoomItemAdapter.kt
+++ app/src/main/java/uz/imes/massageonwifidirect/ui/RoomItemAdapter.kt
@@ -1,6 +1,6 @@
 package uz.imes.imessanger
 
-import android.content.Contextgi
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
